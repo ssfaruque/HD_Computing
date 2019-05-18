56ec6ff6f746d2eab56a6c8e7df9392f4fc223c8
sahil <ssfaruque1@gmail.com>
2019-05-18 12:52:20 -0700

    Bad accuracy

--- food/food_model.py
+++ food/food_model.py
@@ -8,18 +8,24 @@ import pickle
 D = 10000
 rand_indices = rand.sample(range(D), D // 2)
 
-
-def calc_abs_iM(rand_indices, min_hv, index, D, m):
+# generate the iM corresponding to an absorbance on the fly
+def calc_abs_iM(min_hv, abs_val, D, m):
     num_bits_to_flip = math.floor((D / 2) / (m - 1))
     hv = min_hv
 
     start = 0
     end = num_bits_to_flip
 
-    for i in range(index):
-        hv[rand_indices[start : end]] *= -1
-        start = end
-        end += num_bits_to_flip
+    #for i in range(index):
+    #    hv[rand_indices[start : end]] *= -1
+    #    start = end
+    #    end += num_bits_to_flip
+
+    step_size = 1 / m
+    index = round(abs_val / step_size)
+    
+
+    hv[rand_indices[0 : (num_bits_to_flip * index)]] *= -1
 
     return hv
 
@@ -40,7 +46,7 @@ def gen_n_gram_sum(absorbances, min_hv, D, n):
         prod_hv = np.ones(D)
 
         for absorbance in tri_absorbance:
-        	tmp_hv = calc_abs_iM(rand_indices, min_hv, index, D, 3)
+        	tmp_hv = calc_abs_iM(min_hv, absorbance, D, m=1001)
         	absorbance_hv = np.roll(tmp_hv, num_shifts)
         	prod_hv *= absorbance_hv
         	num_shifts -= 1
@@ -72,29 +78,30 @@ def gen_max_hv(start_hv, D):
     return hv
 
 
+marker = 101
+
 class Food_Model(hdc.HD_Model):
 	def __init__(self, D=10000):
 		hdc.HD_Model.__init__(self, D)
 
 	def train(self):
 		#wavenum_step = 1.928816
-		training_set_length = 11
+		training_set_length = marker
 
 		print("Beginning training...")
 
 		for i in range(1, training_set_length):
-			print("Training on file:{}".format(self.dataset[i][1]))
-			label       = int(self.dataset[i][0])
-			absorbances = self.dataset[i][2:]
-			absorbances = list(map(float, absorbances))
+			if "inliquidLive" in self.dataset[i][1]:
+				print("Training on file: {}".format(self.dataset[i][1]))
+				label       = int(self.dataset[i][0])
+				absorbances = self.dataset[i][2:]
+				absorbances = list(map(float, absorbances))
 
+				if label not in self.AM:
+					self.AM[label] = np.zeros(self.D)
 
-
-			if label not in self.AM:
-				self.AM[label] = np.zeros(self.D)
-
-			ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.D, n=3)
-			self.AM[label] += binarizeHV(ngram_sum, 0)
+				ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.D, n=25)
+				self.AM[label] += binarizeHV(ngram_sum, 0)
 
 		# binarize the AMs
 		for key in self.AM:
@@ -109,24 +116,25 @@ class Food_Model(hdc.HD_Model):
 		correct = 0
 
 
-		for i in range(101, testing_set_length):
-		    print("Testing on file:{}".format(self.dataset[i][1]))
-		    label       = int(self.dataset[i][0])
-		    absorbances = self.dataset[i][2:]
-		    absorbances = list(map(float, absorbances))
+		for i in range(marker, testing_set_length):
+			if "inliquidLive" in self.dataset[i][1]:
+			    print("Testing on file:{}".format(self.dataset[i][1]))
+			    label       = int(self.dataset[i][0])
+			    absorbances = self.dataset[i][2:]
+			    absorbances = list(map(float, absorbances))
 
-		    ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.D, n=3)
-		    query_hv = binarizeHV(ngram_sum, 0)
-		    predicted = self.query(query_hv)
-		    
-		    print("predicted: {}, ground truth: {}".format(predicted, label))
+			    ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.D, n=25)
+			    query_hv = binarizeHV(ngram_sum, 0)
+			    predicted = self.query(query_hv)
+			    
+			    print("predicted: {}, ground truth: {}".format(predicted, label))
 
-			
+				
 
-		    if predicted == label:
-		        correct += 1
+			    if predicted == label:
+			        correct += 1
 
-		    total += 1
+			    total += 1
 
 		accuracy = correct / total
 
@@ -138,10 +146,11 @@ class Food_Model(hdc.HD_Model):
 	def load_dataset(self):
 		file = open(sys.argv[1], "r")
 		self.dataset = file.read().splitlines()
+		self.dataset = self.dataset[1:]
 		for i in range(0, len(self.dataset)):
 			self.dataset[i] = self.dataset[i].split(",")
 
-		#rand.shuffle(self.dataset)
+		rand.shuffle(self.dataset)
 
 
 def save(obj, file_name):
