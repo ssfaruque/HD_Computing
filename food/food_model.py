79641c0b5b84dffd6c38adfbd28c003d449cfdc1
ssfaruque <ssfaruque1@gmail.com>
2019-05-20 16:04:43 -0700

    Fixed python reference issue, need deep copy

--- food/food_model.py
+++ food/food_model.py
@@ -11,28 +11,29 @@ rand_indices = rand.sample(range(D), D // 2)
 # generate the iM corresponding to an absorbance on the fly
 def calc_abs_iM(min_hv, abs_val, D, m):
     num_bits_to_flip = math.floor((D / 2) / (m - 1))
-    hv = min_hv
+    hv = np.copy(min_hv)
 
-    start = 0
-    end = num_bits_to_flip
+    step_size = 1 / (m - 1)
+    level = round(abs_val / step_size)
 
-    #for i in range(index):
-    #    hv[rand_indices[start : end]] *= -1
-    #    start = end
-    #    end += num_bits_to_flip
-
-    step_size = 1 / m
-    index = round(abs_val / step_size)
-    
-
-    hv[rand_indices[0 : (num_bits_to_flip * index)]] *= -1
+    hv[rand_indices[0 : (num_bits_to_flip * level)]] *= -1
 
     return hv
 
 
+def calc_wn_iM(min_hv, index, D, m):
+	num_bits_to_flip = math.floor((D / 2) / (m - 1))
+	hv = np.copy(min_hv)
+
+	level = index
+
+	hv[rand_indices[0 : (num_bits_to_flip * level)]] *= -1
+
+	return hv
 
 
-def gen_n_gram_sum(absorbances, min_hv, D, n):
+
+def gen_n_gram_sum(absorbances, min_abs_hv, min_wn_hv, D, n):
     start = 0
     end = n
     index = 0
@@ -40,25 +41,15 @@ def gen_n_gram_sum(absorbances, min_hv, D, n):
     sum_hv = np.zeros(D)
 
     while end < len(absorbances) - n + 1:
-        tri_absorbance = absorbances[start:end]
-        num_shifts = n - 1
-
-        prod_hv = np.ones(D)
-
-        for absorbance in tri_absorbance:
-        	tmp_hv = calc_abs_iM(min_hv, absorbance, D, m=1001)
-        	absorbance_hv = np.roll(tmp_hv, num_shifts)
-        	prod_hv *= absorbance_hv
-        	num_shifts -= 1
-        	index += 1
-
-        index -= 2
-        sum_hv += prod_hv
-        start += 1
-        end += 1
-
+    	absorbance_hv = calc_abs_iM(min_abs_hv, absorbances[index], D, m=1001)
+    	wavenum_hv = calc_wn_iM(min_wn_hv, index, D, m=(len(absorbances) + 1))
+    	sum_hv += (absorbance_hv * wavenum_hv)
+    	index += 1
+    	start += 1
+    	end += 1
+	
     return sum_hv
-
+	
 
 def binarizeHV(hv, threshold):
     for i in range(len(hv)):
@@ -72,16 +63,15 @@ def binarizeHV(hv, threshold):
 
 
 def gen_max_hv(start_hv, D):
-    indices = rand.sample(range(D), D // 2)
     hv = np.copy(start_hv)
-    hv[indices[0 : int(D / 2)]] *= -1
+    hv[rand_indices[0 : int(D / 2)]] *= -1
     return hv
 
 
-marker = 101
+marker = 251
 
 class Food_Model(hdc.HD_Model):
-	def __init__(self, D=10000):
+	def __init__(self, D):
 		hdc.HD_Model.__init__(self, D)
 
 	def train(self):
@@ -90,7 +80,7 @@ class Food_Model(hdc.HD_Model):
 
 		print("Beginning training...")
 
-		for i in range(1, training_set_length):
+		for i in range(0, training_set_length):
 			if "inliquidLive" in self.dataset[i][1]:
 				print("Training on file: {}".format(self.dataset[i][1]))
 				label       = int(self.dataset[i][0])
@@ -100,12 +90,16 @@ class Food_Model(hdc.HD_Model):
 				if label not in self.AM:
 					self.AM[label] = np.zeros(self.D)
 
-				ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.D, n=25)
-				self.AM[label] += binarizeHV(ngram_sum, 0)
+				ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.iM["wavenum_start"], self.D, n=1)
+				self.AM[label] += ngram_sum
+
 
 		# binarize the AMs
 		for key in self.AM:
 			self.AM[key] = binarizeHV(self.AM[key], 0)
+
+		print(self.AM)
+
 
 
 	def test(self):
@@ -116,20 +110,18 @@ class Food_Model(hdc.HD_Model):
 		correct = 0
 
 
-		for i in range(marker, testing_set_length):
+		for i in range(0, marker):
 			if "inliquidLive" in self.dataset[i][1]:
 			    print("Testing on file:{}".format(self.dataset[i][1]))
 			    label       = int(self.dataset[i][0])
 			    absorbances = self.dataset[i][2:]
 			    absorbances = list(map(float, absorbances))
 
-			    ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.D, n=25)
+			    ngram_sum = gen_n_gram_sum(absorbances, self.iM["absorbance_start"], self.iM["wavenum_start"], self.D, n=1)
 			    query_hv = binarizeHV(ngram_sum, 0)
 			    predicted = self.query(query_hv)
 			    
 			    print("predicted: {}, ground truth: {}".format(predicted, label))
-
-				
 
 			    if predicted == label:
 			        correct += 1
@@ -150,7 +142,7 @@ class Food_Model(hdc.HD_Model):
 		for i in range(0, len(self.dataset)):
 			self.dataset[i] = self.dataset[i].split(",")
 
-		rand.shuffle(self.dataset)
+		#rand.shuffle(self.dataset)
 
 
 def save(obj, file_name):
@@ -170,13 +162,16 @@ def main():
 	#D = 10000
 	food_model = Food_Model(D)
 	food_model.load_dataset()
-	food_model.gen_iM(["wavenum_start"], D=10000)
-	food_model.gen_iM(["absorbance_start"], D=10000)
-	food_model.iM["absorbance_end"] = gen_max_hv(food_model.iM["absorbance_start"], D)
+	food_model.gen_iM(["wavenum_start"], D)
+	food_model.gen_iM(["absorbance_start"], D)
+	#food_model.iM["absorbance_end"] = gen_max_hv(food_model.iM["absorbance_start"], D)
+
+	
 	food_model.train()
 	save(food_model, "model.bin")
 	food_model.test()
 	
+
 
 
 
