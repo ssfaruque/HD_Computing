d7c03cbe2b5d1770f7356b958746e4634aef653c
Sahil Faruque <ssfaruque@ucdavis.edu>
2019-08-30 01:01:25 -0400

    Add noise simulations script

---
+++ chemometrics/run_noise_simulations.sh
@@ -0,0 +1,24 @@
+#1/bin/bash
+
+output_dir="noise_simulation_output"
+num_runs=10
+
+mkdir -p $output_dir
+
+categories=( "DNA_ECOLI" "Yeast_inliquid HK" "DNA_INLIQUIDDNA" "DNA_DNA@Anod" "Yeast_inliquid Live" )
+#encoding_schemes=( "multiplication" "trigram" "convolution" )
+encoding_schemes=( "multiplication" "trigram" )
+noise_levels=( 1 2 3 )
+
+for category in "${categories[@]}"
+do
+    for level in "${noise_levels[@]}"
+    do
+        for scheme in "${encoding_schemes[@]}"
+        do
+            python hdc_model.py "datasets/noisyDataSets/noisy_dataset_std_0_${level}.csv" "$category" $scheme 4 $num_runs "${output_dir}/${category}_${scheme}_0_${level}.txt"
+        done
+    done
+done
+
+
