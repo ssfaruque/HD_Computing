7aa1697caa5875fbc366016d5d3b81b7da15438a
kkmohammed <kkmohammed@ucdavis.edu>
2019-10-22 13:17:50 -0700

    added number of files per ppm and number of runs

--- chemometrics/decision_tree.py
+++ chemometrics/decision_tree.py
@@ -6,31 +6,76 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+import statistics
 
 # Function importing Dataset
 def importdata():
     balance_data = pd.read_csv(
 'https://raw.githubusercontent.com/ssfaruque/HD_Computing/master/chemometrics/datasets/DTreeSets/'+
-'noisySets/DT_noisy_005_'+
+#
+#select only one from below
+#'noisySets/DT_noisy_005_'+
 #'noisySets/DT_noisy_01_'+
 #'noisySets/DT_noisy_015_'+
 #'noisySets/DT_noisy_02_'+
 #'noisySets/DT_noisy_03_'+
+#'noisySets/DT_multiplicative_075_'
+#'noisySets/DT_multiplicative_090_'
+#'noisySets/DT_multiplicative_110_'
+#'noisySets/DT_multiplicative_125_'
+#'noisySets/DT_additive_025_'
+#'noisySets/DT_additive_050_'
+#'noisySets/DT_additive_100_'
+#
+#select only one from below
 #'DNA_Anodisc.csv',
 #'DNA_ECOLI.csv',
-#'DNA_inLiquidDNA.csv',
+'DNA_inLiquidDNA.csv',
 #'Full_Set.csv',
 #'Yeast_inLiquidHK.csv',
-'Yeast_inLiquidLive.csv',
+#'Yeast_inLiquidLive.csv',
     sep= ',', header = None)
 
-    # Printing the dataswet shape
-    print ("Dataset Length: ", len(balance_data))
-    print ("Dataset Shape: ", balance_data.shape)
-
-    # Printing the dataset obseravtions
-    print ("Dataset: ",balance_data.head())
+    balance_data = np.array(balance_data)
+    np.random.shuffle(balance_data)
+    threshold = 0.0875
+    for i in range(len(balance_data)):
+        for j in range(1, len(balance_data[0])):
+         if float(balance_data[i][j]) < threshold:
+             balance_data[i][j] = 0
     return balance_data
+
+def retrieve_indices_of_label(balance_data, label):
+    indices = []
+    for i in range(0, len(balance_data[:, 0])):
+        if int(balance_data[i][0]) == label:
+            indices.append(i)
+    return indices
+
+def update_train_test_sets(balance_data):
+    num_files_per_category = 5
+    ppm0  = retrieve_indices_of_label(balance_data, 0)
+    ppm2  = retrieve_indices_of_label(balance_data, 2)
+    ppm5  = retrieve_indices_of_label(balance_data, 5)
+    ppm10 = retrieve_indices_of_label(balance_data, 10)
+    ppm15 = retrieve_indices_of_label(balance_data, 15)
+
+    ppm0_indices  = ppm0[ 0 : num_files_per_category]
+    ppm2_indices  = ppm2[ 0 : num_files_per_category]
+    ppm5_indices  = ppm5[ 0 : num_files_per_category]
+    ppm10_indices = ppm10[0 : num_files_per_category]
+    ppm15_indices = ppm15[0 : num_files_per_category]
+
+    ppm0_samples  = np.copy(balance_data[ppm0_indices])
+    ppm2_samples  = np.copy(balance_data[ppm2_indices])
+    ppm5_samples  = np.copy(balance_data[ppm5_indices])
+    ppm10_samples = np.copy(balance_data[ppm10_indices])
+    ppm15_samples = np.copy(balance_data[ppm15_indices])
+
+    trainset = np.concatenate((ppm0_samples, ppm2_samples, ppm5_samples, ppm10_samples, ppm15_samples))
+    testset = np.delete(balance_data, (ppm0_indices + ppm2_indices + ppm5_indices + ppm10_indices + ppm15_indices), axis=0)
+
+    return trainset, testset
 
 # Function to split the dataset
 def splitdataset(balance_data):
@@ -92,66 +137,125 @@ def cal_accuracy(y_test, y_pred):
 # Driver code
 def main():
 
-    # Building Phase
-    data = importdata()
-    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
-    clf_gini = train_using_gini(X_train, X_test, y_train)
-    clf_entropy = train_using_entropy(X_train, X_test, y_train)
+    predicted_gini_accuracy = []
+    predicted_gini_f1 = []
+    predicted_entropy_accuracy = []
+    predicted_entropy_f1 = []
 
-    # Operational Phase
-    print("Results Using Gini Index:")
+    for i in range(10):
+        # Building Phase
+        data = importdata()
+        #X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+        trainset, testset = update_train_test_sets(data)
+        clf_gini = train_using_gini(trainset[:, 1:1608], testset[:, 1:1608], trainset[:, 0])
+        clf_entropy = train_using_entropy(trainset[:, 1:1608], testset[:,1:1608], trainset[:, 0])
 
-    # Prediction using gini
-    y_pred_gini = prediction(X_test, clf_gini)
-    cal_accuracy(y_test, y_pred_gini)
-    TN = 0
-    TP = 0
-    FN = 0
-    FP = 0
-    for i in range(0,len(y_test)):
-        predicted = y_pred_gini[i]
-        label = y_test[i]
+        # Operational Phase
+        print("Results Using Gini Index:")
 
-        if predicted == label:
-            if predicted == 0 or predicted == 2:
-                TN += 1
+        # Prediction using gini
+        y_pred_gini = prediction(testset[:, 1:1608], clf_gini)
+        cal_accuracy(testset[:, 0], y_pred_gini)
+        TN = 0
+        TP = 0
+        FN = 0
+        FP = 0
+        for i in range(0,len(testset[:,0])):
+            predicted = y_pred_gini[i]
+            label = testset[i][0]
+
+            if predicted == label:
+                if predicted == 0 or predicted == 2:
+                    TN += 1
+                else:
+                    TP += 1
             else:
-                TP += 1
-        else:
-            if predicted == 0:
-                if label == 2:
+                if predicted == 0:
+                    if label == 2:
+                        TN += 1
+                    else:
+                        FN += 1
+                elif predicted == 2:
+                    if label == 0:
+                        TN += 1
+                    else:
+                        FN += 1
+                elif predicted == 5:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TP += 1
+                elif predicted == 10:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TN += 1
+                elif predicted == 15:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TP += 1
+        f1_score = 2 * TP / (2 * TP + FP + FN)
+        print("F1-score: {}\n".format(round(f1_score,2)))
+        predicted_gini_accuracy.append(accuracy_score(testset[:, 0], y_pred_gini)*100)
+        predicted_gini_f1.append(f1_score)
+
+        print("Results Using Entropy:")
+        # Prediction using entropy
+        y_pred_entropy = prediction(testset[:, 1:1608], clf_entropy)
+        cal_accuracy(testset[:, 0], y_pred_entropy)
+        TN = 0
+        TP = 0
+        FN = 0
+        FP = 0
+        for i in range(0,len(testset[:,0])):
+            predicted = y_pred_entropy[i]
+            label = testset[i][0]
+
+            if predicted == label:
+                if predicted == 0 or predicted == 2:
                     TN += 1
-                else:
-                    FN += 1
-            elif predicted == 2:
-                if label == 0:
-                    TN += 1
-                else:
-                    FN += 1
-            elif predicted == 5:
-                if label == 0 or label == 2:
-                    FP += 1
                 else:
                     TP += 1
-            elif predicted == 10:
-                if label == 0 or label == 2:
-                    FP += 1
-                else:
-                    TN += 1
-            elif predicted == 15:
-                if label == 0 or label == 2:
-                    FP += 1
-                else:
-                    TP += 1
-    f1_score = 2 * TP / (2 * TP + FP + FN)
+            else:
+                if predicted == 0:
+                    if label == 2:
+                        TN += 1
+                    else:
+                        FN += 1
+                elif predicted == 2:
+                    if label == 0:
+                        TN += 1
+                    else:
+                        FN += 1
+                elif predicted == 5:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TP += 1
+                elif predicted == 10:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TN += 1
+                elif predicted == 15:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TP += 1
+        F1_Score = 2 * TP / (2 * TP + FP + FN)
+        print("F1-score: {}\n".format(round(F1_Score,2)))
+        predicted_entropy_accuracy.append(accuracy_score(testset[:, 0], y_pred_entropy)*100)
+        predicted_entropy_f1.append(F1_Score)
 
-    print("Results Using Entropy:")
-    # Prediction using entropy
-    y_pred_entropy = prediction(X_test, clf_entropy)
-    cal_accuracy(y_test, y_pred_entropy)
-    report = classification_report(y_test, y_pred_entropy, output_dict = True)
-    print("Test F1: {}".format(round(f1_score,2)))#report["weighted avg"]["f1-score"]))
-
+    Average_Gini_Acc = statistics.mean(predicted_gini_accuracy)
+    Average_Gini_F1 = statistics.mean(predicted_gini_f1)
+    Average_Entropy_Acc = statistics.mean(predicted_entropy_accuracy)
+    Average_Entropy_F1 = statistics.mean(predicted_entropy_f1)
+    print("Average Gini Accuracy: {}".format(round(Average_Gini_Acc, 2)))
+    print("Average Gini F1: {}\n".format(round(Average_Gini_F1, 2)))
+    print("Average Entropy Accuracy: {}".format(round(Average_Entropy_Acc, 2)))
+    print("Average Entropy F1: {}\n".format(round(Average_Entropy_F1, 2)))
 
 # Calling main function
 if __name__=="__main__":
--- chemometrics/gbdt.py
+++ chemometrics/gbdt.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
 from catboost import CatBoostClassifier
+import statistics
 
 # Function importing Dataset
 def importdata():
@@ -35,13 +36,46 @@ def importdata():
 #'Yeast_inLiquidLive.csv',
     sep= ',', header = None)
 
-    # Printing the dataswet shape
-    print ("Dataset Length: ", len(balance_data))
-    print ("Dataset Shape: ", balance_data.shape)
-
-    # Printing the dataset obseravtions
-    print ("Dataset: ",balance_data.head())
+    balance_data = np.array(balance_data)
+    np.random.shuffle(balance_data)
+    threshold = 0.0875
+    for i in range(len(balance_data)):
+        for j in range(1, len(balance_data[0])):
+         if float(balance_data[i][j]) < threshold:
+             balance_data[i][j] = 0
     return balance_data
+
+def retrieve_indices_of_label(balance_data, label):
+    indices = []
+    for i in range(0, len(balance_data[:, 0])):
+        if int(balance_data[i][0]) == label:
+            indices.append(i)
+    return indices
+
+def update_train_test_sets(balance_data):
+    num_files_per_category = 5
+    ppm0  = retrieve_indices_of_label(balance_data, 0)
+    ppm2  = retrieve_indices_of_label(balance_data, 2)
+    ppm5  = retrieve_indices_of_label(balance_data, 5)
+    ppm10 = retrieve_indices_of_label(balance_data, 10)
+    ppm15 = retrieve_indices_of_label(balance_data, 15)
+
+    ppm0_indices  = ppm0[ 0 : num_files_per_category]
+    ppm2_indices  = ppm2[ 0 : num_files_per_category]
+    ppm5_indices  = ppm5[ 0 : num_files_per_category]
+    ppm10_indices = ppm10[0 : num_files_per_category]
+    ppm15_indices = ppm15[0 : num_files_per_category]
+
+    ppm0_samples  = np.copy(balance_data[ppm0_indices])
+    ppm2_samples  = np.copy(balance_data[ppm2_indices])
+    ppm5_samples  = np.copy(balance_data[ppm5_indices])
+    ppm10_samples = np.copy(balance_data[ppm10_indices])
+    ppm15_samples = np.copy(balance_data[ppm15_indices])
+
+    trainset = np.concatenate((ppm0_samples, ppm2_samples, ppm5_samples, ppm10_samples, ppm15_samples))
+    testset = np.delete(balance_data, (ppm0_indices + ppm2_indices + ppm5_indices + ppm10_indices + ppm15_indices), axis=0)
+
+    return trainset, testset
 
 # Function to split the dataset
 def splitdataset(balance_data):
@@ -78,14 +112,65 @@ def cal_accuracy(y_test, y_pred):
 # Driver code
 def main():
 
-    #Training Phase
-    data = importdata()
-    X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+    predicted_accuracy = []
+    predicted_f1 = []
 
-    #Testing Phase
-    y_pred = GradientBoost(X_train, X_test, y_train)
-    cal_accuracy(y_test, y_pred)
+    for i in range(10):
+        #Training Phase
+        data = importdata()
+        #X, Y, X_train, X_test, y_train, y_test = splitdataset(data)
+        trainset, testset = update_train_test_sets(data)
+        #Testing Phase
+        y_pred = GradientBoost(trainset[:, 1:1608], testset[:, 1:1608], trainset[:, 0])
+        cal_accuracy(testset[:, 0], y_pred)
+        TN = 0
+        TP = 0
+        FN = 0
+        FP = 0
+        for i in range(0,len(testset[:,0])):
+            predicted = y_pred[i]
+            label = testset[i][0]
 
+            if predicted == label:
+                if predicted == 0 or predicted == 2:
+                    TN += 1
+                else:
+                    TP += 1
+            else:
+                if predicted == 0:
+                    if label == 2:
+                        TN += 1
+                    else:
+                        FN += 1
+                elif predicted == 2:
+                    if label == 0:
+                        TN += 1
+                    else:
+                        FN += 1
+                elif predicted == 5:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TP += 1
+                elif predicted == 10:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TN += 1
+                elif predicted == 15:
+                    if label == 0 or label == 2:
+                        FP += 1
+                    else:
+                        TP += 1
+        f1_score = 2 * TP / (2 * TP + FP + FN)
+        print("F1-score: {}\n".format(round(f1_score, 2)))
+        predicted_accuracy.append(accuracy_score(testset[:, 0], y_pred)*100)
+        predicted_f1.append(f1_score)
+
+    Average_Acc = statistics.mean(predicted_accuracy)
+    Average_F1 = statistics.mean(predicted_f1)
+    print("Average Accuracy: {}".format(round(Average_Acc, 2)))
+    print("Average F1-score: {}".format(round(Average_F1, 2)))
 
 # Calling main function
 if __name__=="__main__":
